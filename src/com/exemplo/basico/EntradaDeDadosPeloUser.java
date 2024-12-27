b1fc44ee9ea26356d1327245caedaa4b84c9c338
João Victor C <joaovictorcs29@gmail.com>
2024-12-27 12:14:43 -0300

    Exercicio Entrada de dados pelo usuario [X].

---
+++ src/com/exemplo/basico/EntradaDeDadosPeloUser.java
@@ -0,0 +1,23 @@
+package com.exemplo.basico;
+
+//Este programa testa a entrada de dados do usuario usando
+// o Scanner, permitindo interatividade.
+
+import java.util.Scanner;
+
+public class EntradaDeDadosPeloUser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Ola, " +nome+ "! Voce tem " +idade+ " anos.");
+
+        scanner.close();
+
+    }
+}
