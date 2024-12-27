eb6a7c6c2005820635d21bc70973e566f202bc70
João Victor C <joaovictorcs29@gmail.com>
2024-12-27 12:06:36 -0300

    Exercicio Manipulacao de Arrays [X].

---
+++ src/com/exemplo/basico/ManipulacaoDeArrays.java
@@ -0,0 +1,17 @@
+package com.exemplo.basico;
+
+//Este programa testa a criacao e manipulacao de arrays em Java, como acessar e modificar elementos.
+
+public class ManipulacaoDeArrays {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+
+        // Acessando elementos do array
+        System.out.println("Primeiro numero: " + numbers[0]);
+        System.out.println("Ultimo numero: " + numbers[4]);
+
+        // Modificando um valor no array
+        numbers[2] = 10;
+        System.out.println("Novo valor na posicao 2: " + numbers[2]);
+    }
+}
