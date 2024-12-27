c393f2ed99bb73a5c63326032bb6f352feb3ecb5
João Victor C <joaovictorcs29@gmail.com>
2024-12-27 11:32:15 -0300

    FuncoesEmetodos atualizado

--- src/com/exemplo/basico/FuncoesEmetodos.java
+++ src/com/exemplo/basico/FuncoesEmetodos.java
@@ -1,5 +1,21 @@
 package com.exemplo.basico;
 
 public class FuncoesEmetodos {
+    public static void main(String[] args) {
+        int soma = addNumbers(10,5);
+        System.out.println("Soma: " + soma);
 
+        String message = greet("Joao");
+        System.out.println(message);
+    }
+
+    //metodo para somar dois numeros
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    //Metodo para criar uma saudacao
+    public static String greet (String name) {
+        return "Ola, " +name+ "!";
+    }
 }
