4794ae8d03e709687837aab55f9e90b1f02dfcc6
João Victor C <joaovictorcs29@gmail.com>
2025-02-12 16:49:59 -0300

    77 - Orientação Objetos - Modificador final pt 01 - Tipo primitivo

---
+++ src/ProgOrientadaAobjetos/ClassesObjetosEconstrutores/DevDojo/Jmodificadorfinal/dominio/OOP -  Modificador final.md
@@ -0,0 +1,6 @@
+# Modificador final
+
+## 77 - Orientação Objetos - Modificador final pt 01 - Tipo primitivo
+- Obrigado a inicializar o atributo
+- Nao pode ter o valor alterado uma vez que o valor foi instanciado.
+- Escrita de Atributos final: Precisa ser tudo em Maiusculo separado por underline, EX: MODIFICADOR_FINAL; 
---
+++ src/ProgOrientadaAobjetos/ClassesObjetosEconstrutores/DevDojo/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,11 @@
+package ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Jmodificadorfinal.test;
+
+import ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
