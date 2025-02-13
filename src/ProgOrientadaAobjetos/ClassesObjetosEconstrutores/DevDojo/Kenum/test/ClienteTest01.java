32b1f4ec05543855858b7db4286c94b2c53631cf
João Victor C <joaovictorcs29@gmail.com>
2025-02-13 16:37:21 -0300

    82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- src/ProgOrientadaAobjetos/ClassesObjetosEconstrutores/DevDojo/Kenum/test/ClienteTest01.java
+++ src/ProgOrientadaAobjetos/ClassesObjetosEconstrutores/DevDojo/Kenum/test/ClienteTest01.java
@@ -2,14 +2,17 @@ package ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Kenum.test;
 
 import ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Kenum.dominio.Cliente;
 import ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Kenum.dominio.TipoCliente;
-
-import static ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Kenum.dominio.Cliente.*;
+import ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Goku", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
         Cliente cliente2 = new Cliente("Goku", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+
     }
 }
