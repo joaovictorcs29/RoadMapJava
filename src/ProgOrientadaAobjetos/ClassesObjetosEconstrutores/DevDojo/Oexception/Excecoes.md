fe44682430e47e76548ac99ea92801f189ecf670
João Victor Costa <joaovictorcs29@gmail.com>
2025-02-22 20:52:08 -0300

    95 - Exceções pt 01 - Errors

---
+++ src/ProgOrientadaAobjetos/ClassesObjetosEconstrutores/DevDojo/Oexception/Excecoes.md
@@ -0,0 +1,4 @@
+# 95 - Execeções pt 01 - Errors
+
+- Error é algo que acontece na JVM que provavelmente não poderemos recuperar em tempo de execução. Para resolver
+  precisamos arrumar o código e executá-lo novamente.
---
+++ src/ProgOrientadaAobjetos/ClassesObjetosEconstrutores/DevDojo/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package ProgOrientadaAobjetos.ClassesObjetosEconstrutores.DevDojo.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
